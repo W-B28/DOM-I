470598488c3f807272c8fd4276f5a83656f43305
William Bell <wtb2804@gmail.com>
2020-03-09 18:46:12 -0400

    March 9, 2020

--- js/index.js
+++ js/index.js
@@ -40,3 +40,87 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+const navServices= document.querySelector('a:nth-of-type(1)');
+navServices.textContent = 'Services';
+
+const navProduct= document.querySelector('a:nth-of-type(2)');
+navProduct.textContent = 'Product';
+
+const navVision= document.querySelector('a:nth-of-type(3)');
+navVision.textContent = 'Vision';
+
+const navFeatures= document.querySelector('a:nth-of-type(4)');
+navFeatures.textContent = 'Features';
+
+const navAbout= document.querySelector('a:nth-of-type(5)');
+navAbout.textContent = 'About';
+
+const navContact= document.querySelector('a:nth-of-type(6)');
+navContact.textContent = 'Contact';
+
+
+const headLine = document.querySelector('h1');
+headLine.innerHTML = 'DOM<br>IS<br>AWESOME';
+
+const cta = document.querySelector('button');
+cta.textContent = 'Get Started';
+
+
+const ctaImage = document.querySelector('#cta-img');
+ctaImage.src = 'img/header-img.png';
+
+
+
+//  Do T
+
+
+
+let features = document.querySelector('h4:nth-of-type(1)');
+features.id = 'features';
+features.textContent = 'FEATURES';
+
+const featuresParag = document.querySelector('p:nth-of-type(1)');
+featuresParag.id = 'featPara';
+featuresParag.textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+
+
+
+// let about = = document.querySelector('h4:nth-of-type(2)');
+let about = document.querySelector("div.top-content > div:nth-child(2) > h4")
+console.log(about)
+about.id = 'about';
+about.textContent = 'ABOUT';
+
+
+let aboutParag = document.querySelector("div.top-content div:nth-child(2) p")
+aboutParag.id = 'aboutPara';
+aboutParag.textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+
+// const topContent = document.getElementsByClass('top-content');
+// topContent.style.flex;
+
+
+const textContent = document.querySelectorAll('.text-content');
+
+let middleImg = document.getElementById('middle-img');
+middleImg.src = 'img/mid-page-accent.jpg'
+
+console.log(textContent)
+
+textContent.forEach((card, i) => {
+  if(i <= 1){
+    card.style.background = 'red';
+  }
+  if(i > 1){
+    card.style.background = 'blue';
+}
+})
+
+// const navServices= document.querySelector('a:nth-of-type(1)');
+// navServices.textContent = 'Services';
+
+// const about = document.querySelector('h4');
+// about.id = 'about';
+// const aboutID = document.getElementById('about');
+// about.textContent = 'ABOUT';
